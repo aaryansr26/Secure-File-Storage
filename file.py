b6b2b5ba9b0b405d662b9ca6a83cb7dedb2726d4
aaryansr26 <aaryansr26@gmail.com>
2022-06-12 14:40:32 +0530

    added functionality

--- file.py
+++ file.py
@@ -3,23 +3,22 @@ from tkinter import filedialog
 
 def openFile():
     filepath = filedialog.askopenfilename()
-    f = open(filepath,'r')
-    content = f.read()
-    f.close()
-    print(content)
-    
+    return filepath
+
+
+
 def openFiles():
     filepath = filedialog.askopenfilenames()
     file_list = list(filepath)
-    print(file_list)
+    return file_list
     
 
-
-window = Tk()
-window.title("File Reader")
-window.geometry("300x300")
-button = Button(text="Open", command=openFile)
-button2 = Button(text="Open Files", command=openFiles)
-button.pack()
-button2.pack()
-window.mainloop()
+def window_input():
+    window = Tk()
+    window.title("File Reader")
+    window.geometry("300x300")
+    button = Button(text="Open", command=openFile)
+    button2 = Button(text="Open Files", command=openFiles)
+    button.pack()
+    button2.pack()
+    window.mainloop()
