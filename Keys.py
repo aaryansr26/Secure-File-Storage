efb3c56d0af82b4ac21870bea7ef8b4891944eb4
aaryansr26 <aaryansr26@gmail.com>
2022-09-19 21:11:06 +0530

    added config

---
+++ Keys.py
@@ -0,0 +1,12 @@
+import os
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+from cryptography.fernet import Fernet
+
+AESGCMKey = AESGCM.generate_key(bit_length=128)
+ChaChaPolyKey = ChaCha20Poly1305.generate_key()
+FernetKey = Fernet.generate_key()
+MultiFernetK1 = Fernet.generate_key()
+MultiFernetK2 = Fernet.generate_key()
+AESnonce = os.urandom(12)
+key_list = [AESGCMKey, ChaChaPolyKey, FernetKey, MultiFernetK1, MultiFernetK2, AESnonce]
